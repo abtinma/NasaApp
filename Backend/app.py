29ed8a891b8dd93af26afa31bb221378640d647b
Daniyal-Arif <daniyalmarif@gmail.com>
2023-10-07 14:25:35 -0400

    added registeration to backend

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, Response
 import psycopg2
 
 # Create a Flask web application instance
@@ -12,32 +12,72 @@ connection = psycopg2.connect(
     database="database_nasa"
 )
 
-# Define a route that returns a JSON response
-@app.route('/', methods=['GET'])
-def get_json_data():
-    data = {
-        'message': 'Hello, World!',
-        'status': 'success'
-    }
-    return jsonify(data)
 
 @app.route('/user', methods=['GET'])
 def get_all_users():
+    try:
+        user_id = request.headers.get('user_id')
+        
+        # Create a connection 
+        cursor = connection.cursor()
+        
+        # Use placeholders in the query to prevent SQL injection
+        select_query = "SELECT * FROM users WHERE user_id = %s;"
+        cursor.execute(select_query, (user_id,))
+        
+        # Fetch all results from the cursor
+        user_list = cursor.fetchall()
 
-    # Create a connection 
-    cursor = connection.cursor()
-    select_query = "SELECT * FROM users;"
-    cursor.execute(select_query)
-    
-    # Fetch all results from the cursor
-    user_list = cursor.fetchall()
+        # Create a JSON response
+        response = jsonify(user_list)
 
-    response = jsonify(user_list)
+        # Set the HTTP status code to 200 (OK)
+        response.status_code = 200
 
-    # You can also set the HTTP status code (optional)
-    response.status_code = 200
+        return response
+    except Exception as e:
+        # Handle any exceptions and return an error response
+        response = jsonify({'error': str(e)})
+        response.status_code = 500
+        return response
 
-    return response
+
+
+@app.route('/signup', methods=['POST'])
+def sign_up():
+    try:
+        user_id = request.headers.get('user_id')
+        name = request.headers.get('name')
+        username = request.headers.get('username')
+        email = request.headers.get('email')
+
+        print(user_id)
+        print(name)
+        print(username)
+        print(email)
+
+        # Create a connection 
+        cursor = connection.cursor()
+
+        # Use placeholders in the query to prevent SQL injection
+        insert_query = """
+            INSERT INTO users (user_id, name, username, email)
+            VALUES (%s, %s, %s, %s);
+        """
+
+        # Pass the values as a tuple to cursor.execute
+        cursor.execute(insert_query, (user_id, name, username, email))
+
+        # Commit the transaction to save the changes to the database
+        connection.commit()
+
+        # Create a response with a success message
+        response = Response('User registered successfully', status=200)
+        return response
+    except Exception as e:
+        # Handle any exceptions and return an error response
+        response = Response(str(e), status=500)
+        return response
 
 # Run the Flask app if this script is executed
 if __name__ == '__main__':
